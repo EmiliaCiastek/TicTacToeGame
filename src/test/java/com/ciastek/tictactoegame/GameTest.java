54b9a419ea7717bb74b93da12bf64a1eec6f60e2
Emilia <emciastek@gmail.com>
2017-11-08 10:46:56 +0100

    Test commit

---
+++ src/test/java/com/ciastek/tictactoegame/GameTest.java
@@ -0,0 +1,12 @@
+package com.ciastek.tictactoegame;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void simpleTest(){
+        assertEquals(true, true);
+    }
+}
