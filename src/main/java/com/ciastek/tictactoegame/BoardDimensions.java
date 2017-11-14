b9cfeaa6c92d14040b1a0e428aa55e3782cbef9b
emcia <emciastek@gmail.com>
2017-11-14 17:27:08 +0100

    BoardDimensions added

---
+++ src/main/java/com/ciastek/tictactoegame/BoardDimensions.java
@@ -0,0 +1,19 @@
+package com.ciastek.tictactoegame;
+
+public class BoardDimensions {
+    private int width;
+    private int height;
+
+    public BoardDimensions(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/test/java/com/ciastek/tictactoegame/BoardDimensionsTest.java
@@ -0,0 +1,26 @@
+package com.ciastek.tictactoegame;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class BoardDimensionsTest {
+
+    @Test
+    public void shouldReturnWidthWhenGetWidth(){
+        int width = 10;
+        int height = 12;
+        BoardDimensions dimensions = new BoardDimensions(width, height);
+
+        assertEquals(dimensions.getWidth(), width);
+    }
+
+    @Test
+    public void shouldReturnHeightWhenGetHeight(){
+        int width = 10;
+        int height = 12;
+        BoardDimensions dimensions = new BoardDimensions(width, height);
+
+        assertEquals(dimensions.getHeight(), height);
+    }
+}
